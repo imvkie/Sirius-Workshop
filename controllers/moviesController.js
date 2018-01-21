734705787ea1f6e3cb4bb7ed83ea73c2ca6d05c2
Vignesh Nandakumar <nvignesh3398@gmail.com>
2018-01-21 15:05:58 +0530

    njs1

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,3 +12,15 @@ exports.getAllMovies = function(req,res,next){
         return res.json(outputJSON);
     });
   }
+
+exports.addNewMovie = function (req, res, next) {
+  var db = dbService.database,
+      movie = req.body,
+      moviesCollection = db.collection("movies");
+
+      moviesCollection.insert(movie).then(function(save_data){
+        return res.json({
+          "isSuccess" : true
+        });
+      });
+}
